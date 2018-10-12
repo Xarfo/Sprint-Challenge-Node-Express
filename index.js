d6e92086c132ffc4e58135ba84664145bb156151
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-12 12:43:05 -0400

    Global Middlewares Plugged into Server Object
    #Comment
    
    All global middlewares used in across endpoints are plugged into the server

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const projectModel = require('./data/helpers/projectModel');
 
 //Instatiates a server object
 const server = express();
+
+//Third party middleware
+//express.json returns json objects of the response
+//All global middlewares that will be used across enpoints must also be plugged into the server
+//cors and helmet middlewares are not used
+
+server.use(express.json(), logger('combined'), cors(), helmet());
