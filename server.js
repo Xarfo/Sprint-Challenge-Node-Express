d0a6b673ec8954b455bce9be423f8538165f0706
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-15 09:51:59 -0400

    Added Action Endpoints
    #Comment
    
    Added GET, POST, PUT and DELETE operation of action routes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/projects', (req, res) => {
         res.status(200).json(projects)
       })
       .catch(err => {
-        res.status(500).json(`${err}: Information not available`);
+        res.status(500).json(`${err}: Cannot Get`);
       });
    
    });
@@ -41,65 +41,101 @@ app.get('/projects/:id', (req, res) => {
         res.status(200).json(projects);
       })
       .catch(err => {
-        res.status(500).json(`${err}: Information not available`);
+        res.status(500).json(`${err}: Cannot GET by Id`);
       });
    });
 
 //Project Endpoint for Create Operation
-app.post('/projects', (req, res, next) => {
+app.post('/projects', (req, res) => {
     const { name, description, completed } = req.body;
     const newProject = { name, description, completed };
+    console.log(newProject);
     projectModel.insert(newProject)
-      .then(
+      .then(newProject => {
         res.status(200).json(newProject)
-      )
+      })
       .catch(err => {
-        console.error(err);
+        console.error(`${err} Cannot POST`);
       });
    });
 
 //Project Endpoint for Update Operation
-app.put('/projects', (req, res, next) => {
+app.put('/projects/:id', (req, res) => {
     const { name, description, id } = req.body;
     const updatedProject = { name, description };
-    projectModel.update(id, updatedProject)
-      .then(
+    projectModel.update(req.params.id, updatedProject)
+      .then(updatedProject => {
         res.status(200).json(updatedProject)
-      )
+      })
       .catch(err => {
-        console.error(err);
+        console.error(`${err} Cannot PUT`);
       });
    });
 
-  //Project Endpoint for Delete Operation
+//Project Endpoint for Delete Operation
+app.delete('/projects/:id', (req, res) => {
+  console.log(req.params); //Test for req.params shape
+  projectModel.remove(req.params.id)
+    .then(removedProject => {
+      res.status(200).json(removedProject);
+    })
+    .catch(err => {
+      res.status(500).json(`${err}: Cannot DELETE`);
+    });
+ });
 
 
-
-
+//Action Routes
  
-   
-
-
-
-
-
 //Action Endpoint for Get Operation
-app.get('/actions', (req, res, next) => {
+app.get('/actions', (req, res) => {
     actionModel.get()
       .then(actions => {
         res.status(200).json(actions)
       })
       .catch(err => {
         res.status(500).json(err)
-      })
+      });
    
-   })
+   });
 
-
-
-
-
-
+//Action Endpoint for Create Operation
+app.post('/actions', (req, res, next) => {
+    const { description, project_id, notes } = req.body;
+    const newPost = { description, project_id, notes };
+    actionModel.insert(newPost)
+      .then(
+        res.status(200).json(newPost)
+      )
+      .catch(err => {
+        console.error(err);
+      })
+  })
+  
+//Action Endpoint for Update Operation  
+app.put('/actions', (req, res, next) => {
+    const { description, id } = req.body;
+    const updatedAction = { description };
+    actionModel.update(id, updatedAction)
+      .then(
+        res.status(200).json(updatedAction)
+      )
+      .catch(err => {
+        console.error(err);
+      })
+  })
+  
+//Action Endpoint for Delete Operation  
+app.delete('/actions', (req, res, next) => {
+    const { id } = req.body;
+    actionModel.remove(id)
+      .then(
+        res.status(200).json(`Action #${id} has been deleted`)
+      )
+      .catch(err => {
+        console.error(err);
+      })
+  })   
 
 
 //Port & Port Listner
